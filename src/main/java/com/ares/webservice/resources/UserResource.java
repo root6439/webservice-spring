7b3a6e4bb08e7bc4e6e910ed1b60810cee630e27
Nicolas Soares <nicolas.soaresh.98@gmail.com>
2020-03-13 17:01:54 -0300

    User entity e User resource criadas

---
+++ src/main/java/com/ares/webservice/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.ares.webservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ares.webservice.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Olavo", "olavo@gmail.com", "23143245", "123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
