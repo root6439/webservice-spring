1ff72a3ffa6287a82dd902f254247d8b88a28496
Nicolas Soares <nicolas.soaresh.98@gmail.com>
2020-03-22 16:39:25 -0300

    Category implementada

---
+++ src/main/java/com/ares/webservice/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ares.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ares.webservice.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
---
+++ src/main/java/com/ares/webservice/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.ares.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ares.webservice.entities.Category;
+import com.ares.webservice.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> cat = repository.findById(id);
+		return cat.get();
+	}
+	
+	
+}
