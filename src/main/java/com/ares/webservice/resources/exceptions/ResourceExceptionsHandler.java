80e84306481889432900205b7b3e3e76a1d4472e
Nicolas Soares <nicolas.soaresh.98@gmail.com>
2020-03-30 11:32:59 -0300

    Tratamento de exceções para getUser

---
+++ src/main/java/com/ares/webservice/resources/exceptions/ResourceExceptionsHandler.java
@@ -0,0 +1,27 @@
+package com.ares.webservice.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ares.webservice.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionsHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError err = new StandardError(Instant.now(), status.value(), error, exception.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
